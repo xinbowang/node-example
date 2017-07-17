6a107315c23c1723610bcbf28608844a73a08dc4
xinbowang <664176494@qq.com>
2017-07-17 10:32:43 +0800

    增加退出

---
+++ public/js/admin.js
@@ -0,0 +1,11 @@
+$('#logout').click(function(){
+	$.ajax({
+		url : '/api/user/logout',
+		success : function(result){
+			if( !result.code ){
+				alert('退出成功！');
+				window.location.href='/'; // 退出成功， 跳转页面
+			}
+		}
+	})
+})
